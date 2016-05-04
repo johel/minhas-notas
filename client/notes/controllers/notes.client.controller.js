fd4fe5841e3d2f8f77d3e01bc4f49726ce75eeac
johel <johel.carvalho@gmail.com>
2016-05-04 10:17:59 -0300

    prevented from saving note when it is exactly equal to original

--- client/notes/controllers/notes.client.controller.js
+++ client/notes/controllers/notes.client.controller.js
@@ -10,11 +10,28 @@ app.controller('notesCtrl', function($scope,$timeout, NoteService){
 		NoteService.delete(note);
 	}
 
+	$scope.edit = function(note) {
+		console.log('clicked edit');
+		$scope.editedNote = note;
+
+		//extend makes a shallow copy of own properties of one or more source objects
+		// here "note" is an object whose properties are not objects so shallow copy is the same as deep copies
+		//ex: var object = angular.extend({}, object1, object2).
+		$scope.originalNote = angular.extend({}, note);
+	}
+
 	$scope.saveEdits = function (note, $event) {
 		console.log('saveEdits');
 		
+		//if esc was pressed
 		if ($scope.reverted) {
 			$scope.reverted = false;
+			return;
+		}
+
+		//if no changes were made to the original text
+		if (note.text === $scope.originalNote.text) {
+			console.log('exactly equal to original');
 			return;
 		}
 
@@ -24,8 +41,6 @@ app.controller('notesCtrl', function($scope,$timeout, NoteService){
 	$scope.revertEdits = function (note) {
 		console.log('evert edits');
 		$scope.reverted = true;
-
-
 
 		/****Other and better solution if it was possible to pass $event to custom onEscape directive just like ng-blur does
 			It is better because you can truly reuse the directive. The way it is implemented just solves our specific problem.
