65d5125efbb8b3cf29aea557d7e6678a6e452f2e
johel <johel.carvalho@gmail.com>
2016-05-04 22:10:14 -0300

    made sure sequelize was synchronized during app launch so tables can exist

--- server.js
+++ server.js
@@ -14,8 +14,10 @@ var app = express();
 
 var passport = passport();
 
-app.listen(3000);
+models.sequelize.sync().then(function () {
+ 	app.listen(3000);
+	console.log('Server running at http://localhost:3000/');
+});
 
-console.log('Server running at http://localhost:3000/');
 
 module.exports = app;
